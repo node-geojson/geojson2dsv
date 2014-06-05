ea5cd3c13eef632e1ebc48e62af087aecdaf1946
James Seppi <james.seppi@gmail.com>
2014-06-05 15:38:30 -0500

    serialize properties of non-Point geometries

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 var dsv = require('dsv');
+var normalize = require('geojson-normalize');
 
 module.exports = function(_, delim) {
-    switch (_.type) {
-        case 'FeatureCollection':
-            break;
-        case 'Feature':
-            _ = { type: 'FeatureCollection', features: [_] };
-            break;
-        case 'Point':
-            _ = { type: 'FeatureCollection', features: [{ type: 'Feature', properties: {}, geometry: _ }] };
-            break;
-        default:
-            return null;
-    }
+    _ = normalize(_);
 
-    return dsv(delim || ',').format(_.features.filter(function(f) {
-        return f.geometry.type == 'Point';
-    }).map(function(f) {
+    var onlyPoints = _.features.every(function(f) {
+        return f.geometry.type === 'Point';
+    });
+
+    return dsv(delim || ',').format(_.features.map(function(f) {
         var p = JSON.parse(JSON.stringify(f.properties));
-        p.lon = f.geometry.coordinates[0];
-        p.lat = f.geometry.coordinates[1];
+        if (onlyPoints) {
+            p.lon = f.geometry.coordinates[0];
+            p.lat = f.geometry.coordinates[1];
+        }
         return p;
     }));
 };
