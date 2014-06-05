cdc891ed864b99a176cb20ed52b23c82d982dfd1
James Seppi <james.seppi@gmail.com>
2014-06-05 15:38:30 -0500

    serialize properties of non-Point geometries

--- test/test.js
+++ test/test.js
@@ -2,53 +2,80 @@ var geojson2dsv = require('../');
 var test = require('tape');
 
 test('geojson2dsv', function(t) {
-    t.equal(geojson2dsv({
+  t.equal(
+    geojson2dsv({
+      type: 'Point',
+      coordinates: [0, 0]
+    }),
+    'lon,lat\n0,0'
+  );
+  t.equal(
+    geojson2dsv(
+      {
         type: 'Point',
-        coordinates: [0,0]
-    }), 'lon,lat\n0,0');
-    t.equal(geojson2dsv({
+        coordinates: [0, 0]
+      },
+      ';'
+    ),
+    'lon;lat\n0;0'
+  );
+  t.equal(
+    geojson2dsv({
+      type: 'Feature',
+      geometry: {
         type: 'Point',
-        coordinates: [0,0]
-    }, ';'), 'lon;lat\n0;0');
-    t.equal(geojson2dsv({
-        type: 'Feature',
-        geometry: {
+        coordinates: [0, 0]
+      },
+      properties: {
+        a: 'b'
+      }
+    }),
+    'a,lon,lat\nb,0,0'
+  );
+  t.equal(
+    geojson2dsv({
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          geometry: {
             type: 'Point',
-            coordinates: [0,0]
-        },
-        properties: {
+            coordinates: [10, 0]
+          },
+          properties: {
             a: 'b'
+          }
         }
-    }), 'a,lon,lat\nb,0,0');
-    t.equal(geojson2dsv({
-        type: 'FeatureCollection',
-        features: [{
-            type: 'Feature',
-            geometry: {
-                type: 'Point',
-                coordinates: [10,0]
-            },
-            properties: {
-                a: 'b'
-            }
-        }]
-    }), 'a,lon,lat\nb,10,0');
-    t.equal(geojson2dsv({
-        type: 'FeatureCollection',
-        features: [{
-            type: 'Feature',
-            geometry: {
-                type: 'Polygon',
-                coordinates: [[10,0]]
-            },
-            properties: {
-                a: 'b'
-            }
-        }]
-    }), '', 'polygon ignore');
-    t.equal(geojson2dsv({
-        type: 'LineString',
-        coordinates: [[10,0], [0, 0]]
-    }), null, 'linestring ignore');
-    t.end();
+      ]
+    }),
+    'a,lon,lat\nb,10,0'
+  );
+  t.equal(
+    geojson2dsv({
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Polygon',
+            coordinates: [[10, 0]]
+          },
+          properties: {
+            a: 'b'
+          }
+        }
+      ]
+    }),
+    '',
+    'polygon ignore'
+  );
+  t.equal(
+    geojson2dsv({
+      type: 'LineString',
+      coordinates: [[10, 0], [0, 0]]
+    }),
+    null,
+    'linestring ignore'
+  );
+  t.end();
 });
